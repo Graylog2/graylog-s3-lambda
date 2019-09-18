5fb4d4704e82129473d508050a98bfd824380eff
Dan Torrey <dan@graylog.com>
2019-09-18 17:28:03 -0500

    Shorten Message field

--- GraylogS3Function/src/main/java/org.graylog.integrations.s3/CloudFlareLogsParser.java
+++ GraylogS3Function/src/main/java/org.graylog.integrations.s3/CloudFlareLogsParser.java
@@ -10,6 +10,9 @@ import org.graylog2.gelfclient.GelfMessage;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CloudFlareLogsParser {
 
@@ -22,7 +25,18 @@ public class CloudFlareLogsParser {
               .disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
 
         HashMap<String, Object> valueMap = mapper.readValue(stringMessage, HashMap.class);
-        final GelfMessage message = new GelfMessage(stringMessage, graylogHost);
+
+        // Prepare message string with a few select fields.
+        final Map<String, Object> messageMap = new LinkedHashMap<>();
+        messageMap.put("ClientRequestHost", valueMap.get("ClientRequestHost"));
+        messageMap.put("ClientRequestPath", valueMap.get("ClientRequestURI"));
+        messageMap.put("OriginIP", valueMap.get("OriginIP"));
+        messageMap.put("ClientSrcPort", valueMap.get("ClientSrcPort"));
+        messageMap.put("EdgeServerIP", valueMap.get("EdgeServerIP"));
+        messageMap.put("EdgeResponseBytes", valueMap.get("EdgeResponseBytes"));
+        final String messageSummary = messageMap.keySet().stream().map(key -> key + ": " + valueMap.get(key)).collect(Collectors.joining(" | "));
+
+        final GelfMessage message = new GelfMessage(messageSummary, graylogHost);
 
         // TODO: Add support for other timestamp formats. Currently only RFC3339 is supported.
         final String timestamp = (String) valueMap.get("EdgeStartTimestamp");
