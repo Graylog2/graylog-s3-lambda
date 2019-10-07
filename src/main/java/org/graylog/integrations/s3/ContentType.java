71a69ef5a6f6760c3ab48c42859880f750e87e7f
Dan Torrey <dan@graylog.com>
2019-10-07 11:42:16 -0500

    Use application/x.cloudflare.logpush mime type

--- src/main/java/org/graylog/integrations/s3/ContentType.java
+++ src/main/java/org/graylog/integrations/s3/ContentType.java
@@ -8,12 +8,18 @@ import java.util.Objects;
 
 /**
  * Configurable content type for the S3 Lambda function.
+ *
+ * Technically, MediaType or MimeType is a more accurate name for this configuration option (since ContentType is
+ * specifically an HTTP construct). However, ContentType is used here, since it is the most clear/well know term.
+ *
+ * See {@link org.graylog.integrations.s3.codec.CodecProcessor} for the log message codec selection.
+ *
  * See {@link org.graylog.integrations.s3.config.Configuration}.
  */
 public enum ContentType {
 
     APPLICATION_JSON("application/json"),
-    CLOUD_FLARE_LOGPUSH("cloudflare/logpush"),
+    CLOUD_FLARE_LOGPUSH("application/x.cloudflare.logpush"), // "x." indicates a custom mime type.
     TEXT_PLAIN("text/plain");
 
     private static final Logger LOG = LogManager.getLogger(ContentType.class);
